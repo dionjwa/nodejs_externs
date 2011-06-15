c5f7c3beb494d18f6fa4762110b53228b5d1af13
Dion Amago <dionjw@gmail.com>
2011-06-15 16:56:29 -0500

    Add nodejs as dependency

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #! /usr/bin/env sh
 ../hydrax/bin/createhaxelib.py haxelib.xml
-haxelib test nodejs_externs.zip
-rm nodejs_externs.zip
-haxelib dev nodejs_externs /Users/dion/storage/projects/nodejs_externs
+# haxelib test nodejs_externs.zip
+# rm nodejs_externs.zip
+# haxelib dev nodejs_externs /Users/dion/storage/projects/nodejs_externs
 
