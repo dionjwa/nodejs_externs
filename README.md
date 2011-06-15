d493e005228ef34223974af362a58dafa62c6e82
dionjwa <dionjw@gmail.com>
2011-06-15 15:51:15 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Node.js externs for HaXe
+
+Repository for the haxelib node_externs
+
+Creating externs to use external modules in Node.js with HaXe often means writing one .hx file with a bunch of typedefs.  I don't think it's worth creating separate haxelibs or repositories for each module extern, so this repository is a collection point.
+
+So far there are externs for:
+	connect
+	everyauth
+	forever
+	v8profiler
+	node-mime
+	node-user-agent
+	
+	
+Note that none of these externs are complete, in that the typedefs don't necessarily define every single variable and function.  They define enough to be useful.  Feel free to contribute additions and improvements. 
---
+++ js/node/Connect.hx
@@ -0,0 +1,49 @@
+package js.node;
+
+import js.Node;
+
+using StringTools;
+using Lambda;
+
+typedef RouterApp = {
+	function get(path :String, onRequest :ServerRequest->ServerResponse->Dynamic->Void) :Void;
+}
+
+typedef OAuth2 = {
+	function myHostname (arg :Dynamic) :OAuth2;
+	function appId (arg :Dynamic) :OAuth2;
+	function appSecret (arg :Dynamic) :OAuth2;
+	function entryPath (arg :Dynamic) :OAuth2;
+	function scope (arg :Dynamic) :OAuth2;
+	function handleAuthCallbackError (arg :Dynamic) :OAuth2;
+	function findOrCreateUser (arg :Dynamic) :OAuth2;
+	function redirectPath (arg :Dynamic) :OAuth2;
+	function apiHost (arg :Dynamic) :OAuth2;
+	function configurable () :Dynamic;
+}
+
+typedef EveryAuth = {
+	var facebook :OAuth2;
+	var google :OAuth2;
+	// function middleware() :MiddleWare;
+	function middleware() :Dynamic;
+}
+
+typedef ConnectServer = {
+	function use (?middlewareMountPoint :Dynamic, middleware :Dynamic) :ConnectServer;
+	function listen (port :Int, ?address :String) :Void;
+}
+
+// typedef Next = Void->MiddleWare;
+// typedef MiddleWare = ServerRequest->ServerResponse->Next->Void;
+typedef MiddleWare = ServerRequest->ServerResponse->Dynamic->Void;
+
+typedef Connect = {
+	function createServer (a1 :Dynamic, ?a2 :Dynamic, ?a3 :Dynamic, ?a4 :Dynamic, ?a5 :Dynamic, ?a6 :Dynamic, ?a7 :Dynamic, ?a8 :Dynamic, ?a9 :Dynamic) :ConnectServer;
+	function cookieParser() :MiddleWare;
+	function bodyParser() :MiddleWare;
+	function session(params :Dynamic) :Void;
+	function router(routes :Dynamic->Void) :Void;
+	function Static (path :String, ?options :Dynamic) :MiddleWare; 
+	function errorHandler (options :Dynamic) :MiddleWare;
+}
