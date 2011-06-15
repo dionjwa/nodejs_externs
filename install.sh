e8bd2a4508a4912c042496082c8745dc699fad2d
Dion Amago <dionjw@gmail.com>
2011-06-15 16:57:40 -0500

    Remove local install.sh file

--- install.sh
+++
@@ -1,6 +0,0 @@
-#! /usr/bin/env sh
-../hydrax/bin/createhaxelib.py haxelib.xml
-# haxelib test nodejs_externs.zip
-# rm nodejs_externs.zip
-# haxelib dev nodejs_externs /Users/dion/storage/projects/nodejs_externs
-
