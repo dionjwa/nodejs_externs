c07e234556677d6c0a8f56251602e8acf709bb39
sledorze <stephane.ledorze@gmail.com>
2011-10-12 22:57:36 +0200

    add Express, FacebookClient, NodeUuid, SocketIo partial signatures + added a variant of Connect listen function

---
+++ js/node/Express.hx
@@ -0,0 +1,46 @@
+package js.node;
+
+import js.node.Connect;
+import js.Node;
+import js.node.EveryAuth;
+
+/**
+ * ...
+ * @author sledorze
+ */
+
+typedef ExpressHttpServerReq = { > NodeHttpServerReq,
+	var session : Session;
+}
+
+typedef ExpressHttpServerResp = { > NodeHttpServerResp,
+	function render(name : String, params : Dynamic) : Void;
+	function redirect(url : String) : Void;
+}
+ 
+ 
+typedef ExpressServer = {
+	function use (?middlewareMountPoint :Dynamic, middleware :Dynamic) :ConnectServer;
+	function get(path : String, f : ExpressHttpServerReq ->  ExpressHttpServerResp -> Void) : Void;
+
+	@:overload(function(port :Int, ready : Void -> Void):Void { } )
+	function listen (port :Int, ?address :String) :Void;
+}
+
+typedef Express = {
+
+	function createServer (a1 :Dynamic, ?a2 :Dynamic, ?a3 :Dynamic, ?a4 :Dynamic, ?a5 :Dynamic, ?a6 :Dynamic, ?a7 :Dynamic, ?a8 :Dynamic, ?a9 :Dynamic) :ExpressServer;
+
+	function cookieParser() :MiddleWare;
+	function bodyParser() :MiddleWare;
+	function session(params :Dynamic) :Void;
+	function router(routes :Dynamic->Void) :Void;
+	function Static (path :String, ?options :Dynamic) :MiddleWare; 
+	function errorHandler (options :Dynamic) :MiddleWare;	
+
+	function logger() : MiddleWare;
+	@:native("static") function static_(path : String, ?option : Dynamic) : MiddleWare;
+	
+}
+
+// class ExpressExtensions 
---
+++ js/node/JsHelper.hx
@@ -0,0 +1,19 @@
+package js.node;
+import js.Node;
+
+/**
+ * ...
+ * @author sledorze
+ */
+
+class JsHelper {
+
+	inline public static function new_<T>(x : T) : T return
+		untyped __js__("new x()")
+	
+	public static function requireAs<T>(name : String, cl : Class<T>) : T return
+		untyped js.Node.require(name)
+
+	inline public static function ifNull<T>(alt : T, v : T) : T return
+		v == null?alt:v
+}
